bb9ea7d8c249232d3b719e91c6a4cb8aea9d2796
krelk_works <leo.craciun97@gmail.com>
2024-05-18 23:07:41 +0200

    Ultims detalls

--- src/App.java
+++ src/App.java
@@ -5,7 +5,13 @@ import MySQL.*;
 public class App {
     public static void main(String[] args) {
         try {
+            // Afegir aqui les dades de la teva base de dades
+
             SQLController controlSQL = new SQLController("localhost", "root", "", "agendaJAVA");
+
+            // --------------------------------------------------------------------------------------------------------
+            
+            // Si tot va bé, s'obre la finestra de l'agenda
             new AgendaGUI(controlSQL);
         } catch (SQLException e) {
             System.err.println("Error amb la conexió a la base de dades: " + e.getMessage());
